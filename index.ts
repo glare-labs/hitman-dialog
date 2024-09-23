f65af4a657f07f1c56ce68c03893353446deecb7
bre97-web <bre97-web@outlook.com>
2024-09-23 16:27:28 +0800

    chore: update package.json and build config

---
+++ index.ts
@@ -0,0 +1,4 @@
+export * from './dialog-button/hitman-dialog-button.js'
+export * from './dialog-header/hitman-dialog-header.js'
+export * from './dialog/hitman-dialog.js'
+
--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,27 @@
 import nodeResolve from '@rollup/plugin-node-resolve'
+import terser from '@rollup/plugin-terser'
 import typescript from "@rollup/plugin-typescript"
 
 export default {
-    input: ['./src/index.ts'],
-    output: [
-        {
-            file: './build/index.js',
-            format: "esm",
-        },
+    input: [
+        './index.ts',
+        './dialog/hitman-dialog.ts',
+        './dialog-button/hitman-dialog-button.ts',
+        './dialog-header/hitman-dialog-header.ts',
     ],
+    output: {
+        format: "esm",
+        entryFileNames: '[name].js',
+        dir: 'build',
+        preserveModules: true
+    },
     plugins: [
         nodeResolve(),
         typescript({
             tsconfig: './tsconfig.json',
             outputToFilesystem: false,
         }),
+        terser()
     ],
     watch: true,
 }
